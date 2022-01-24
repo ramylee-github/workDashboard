36b41176477499a2893c2444dbc4cdec12ab01d6
BORAMLEE <boram1073@gmail.com>
2022-01-24 17:59:32 +0900

    Design: 추가 마크업

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,19 +1,22 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
-import del from "del";
-import ws from "gulp-webserver";
-import image from "gulp-image";
-import dsass from "sass";
 import gsass from "gulp-sass";
 import autoprefixer from "gulp-autoprefixer";
 import miniCSS from "gulp-csso";
+import image from "gulp-image";
 import browserify from "gulp-bro";
+import del from "del";
+import dsass from "sass";
 import babelify from "babelify";
+// import gsourcemap from "gulp-sourcemap";
+import grename from "gulp-rename";
+import ws from "gulp-webserver";
 
-const sass = gsass(dsass);
-
-// sass.compiler = require("node-sass");
-
+/*
+ * ========================
+ * each elements's route
+ * =======================
+*/
 const routes = {
     pug: {
         watch:"src/**/*.pug",
@@ -41,25 +44,31 @@ const routes = {
     }
 };
 
+/*
+ * ====================================================
+ *  delete build folder before new build is generated
+ * ===================================================
+*/
+const clean = () => del(["build"]);
+
+/*
+ * ==============
+ *  PUG to HTML
+ * ==============
+*/
 const pug = () => 
     gulp
     .src(routes.pug.src)
     .pipe(gpug())
+    // .pipe(gsourcemap.write())
     .pipe(gulp.dest(routes.pug.dest));
 
-const clean = () => del(["build"]);
-
-const webserver = () => 
-    gulp
-    .src("build")
-    .pipe(ws({livereload: true, open: true}));
-
-
-const img = () =>  
-    gulp.src(routes.img.src)
-    .pipe(image())
-    .pipe(gulp.dest(routes.img.dest));
-
+/*
+ * =======================
+ *  scss to css + minify
+ * =======================
+*/
+const sass = gsass(dsass);
 const styles = () =>  
     gulp.src(routes.scss.src)
     .pipe(sass.sync().on('error', sass.logError))
@@ -68,7 +77,7 @@ const styles = () =>
     }))
     .pipe(miniCSS())
     .pipe(gulp.dest(routes.scss.dest));
-
+//for library styles
 const libStyles = () =>  
     gulp.src(routes.libScss.src)
     .pipe(sass().on('error', sass.logError))
@@ -78,6 +87,11 @@ const libStyles = () =>
     .pipe(miniCSS())
     .pipe(gulp.dest(routes.libScss.dest));
 
+/*
+ * ============================
+ *  transpiling and minify js
+ * ============================
+*/
 const js = () =>  
     gulp.src(routes.js.src)
     .pipe(browserify({
@@ -86,21 +100,47 @@ const js = () =>
             ["uglifyify", {global: true}],
         ]
     }))
+    .pipe(grename('main.min.js'))
     .pipe(gulp.dest(routes.js.dest));
 
+/*
+ * ================
+ *  minify images
+ * ================
+*/
+const img = () =>  
+    gulp.src(routes.img.src)
+    .pipe(image())
+    .pipe(gulp.dest(routes.img.dest));
 
+/*
+ * ==============================
+ *  setting a live local server
+ * =============================
+*/
+const webserver = () => 
+    gulp
+    .src("build")
+    .pipe(ws({livereload: true, open: true}));
+
+/*
+ * =========================================
+ *  setting real time watching environment
+ * ========================================
+*/
 const watch = () => {
     gulp.watch(routes.pug.watch, pug);
-    gulp.watch(routes.img.src, img);//이미지의 변동이 있을때마다 최적화 과정을 실행. 큰 사이즈 이미지가 있을경우 시간이 오래걸릴 수 있음.
+    gulp.watch(routes.img.src, img);
     gulp.watch(routes.scss.watch, styles);
     gulp.watch(routes.libScss.watch, libStyles);
     gulp.watch(routes.js.watch, js);
 };
 
+//prepare step
 const prepare = gulp.series([clean, img]);
-
+//core step
 const assets = gulp.series([pug, styles, js]);
-
+//extra step for develop environment and automation
 const postDev = gulp.parallel([webserver, watch]);
 
 export const dev = gulp.series([prepare, assets, postDev]);
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,357 @@
-const rOne = random(10);
-const rTwo = random(20);
+'use strict';
+charts();
+quickMenu();
 
-console.log( `${rOne} ${rTwo}` ) ;
+
+function quickMenu() {
+    const quickMenu = document.querySelector('#quickMenu');
+
+    quickMenu.addEventListener('mouseenter', function(e) {
+        this.classList.add('quick-menu--opened');
+    });
+    
+    quickMenu.addEventListener('mouseleave', function(e) {
+        this.classList.remove('quick-menu--opened');
+    });
+}
+
+function charts() {
+    stackedBarChart('chart01');
+    barLineChart('chart02');
+    spiderChart('chart03');
+    lineChart('chart04');
+
+    stackedBarChart = (chartId) => {
+        Highcharts.chart(chartId, {
+            chart: {
+                type: 'column',
+                style: {
+                    fontFamily:"oneshinhan"
+                },
+                margin: [20,10, 25, 60]
+            },
+            title: {
+                text: ''
+            },
+            credits: {
+                enabled: false
+            },
+            xAxis: {
+                categories: [
+                    '7월',
+                    '8월',
+                    '9월'
+                ],
+                lineColor:'#d7d7d7',
+                endOnTick: true,
+                startOnTick: true,
+                tickColor:'#d7d7d7',
+                tickWidth: 1,
+                labels: {
+                    style: {
+                    color:'#888',
+                    fontSize:'12px'
+                    }
+                }
+            },
+            yAxis: [{
+                min: 0,
+                title: {
+                    text: '',
+                    style: {
+                        color:'#aaa',
+                        fontSize:'12px',
+                    }
+                },
+                labels: {
+                    style: {
+                    color:'#aaa',
+                    fontSize:'10px',
+                    }
+                },
+                gridLineColor:'#f1f1f1'
+            }, {
+                title: {
+                    text: ''
+                },
+                opposite: true
+            }],
+            legend: {
+                enabled: false
+            },
+            tooltip: {
+                shared: true
+            },
+            plotOptions: {
+                column: {
+                    grouping: false,
+                    shadow: false,
+                    borderWidth: 0,
+                    
+                }
+            },
+            exporting: {
+                enabled:false
+                },
+            series: [{
+                name: '목표',
+                color: '#e3dffb',
+                data: [150, 73, 20],
+                pointWidth:37
+            }, {
+                name: '발생',
+                    color: '#715deb',
+                data: [140, 90, 40],
+                pointWidth:21
+            }]
+        });
+    }
+    barLineChart = (chartId) => {
+        Highcharts.chart(chartId, {
+            chart: {
+                type: 'column',
+                style: {
+                    fontFamily:"oneshinhan"
+                },
+                margin: [20, 30, 25, 60]
+            },
+            title: {
+                text: ''
+            },
+            credits: {
+                enabled: false
+            },
+            xAxis: {
+                categories: [
+                    '7월',
+                    '8월',
+                    '9월'
+                ],
+                lineColor:'#d7d7d7',
+                endOnTick: true,
+                startOnTick: true,
+                tickColor:'#d7d7d7',
+                tickWidth: 1,
+                labels: {
+                    style: {
+                    color:'#888',
+                    fontSize:'12px'
+                    }
+                }
+            },
+            yAxis: [{
+                min: 0,
+                title: {
+                    text: '',
+                    style: {
+                        color:'#aaa',
+                        fontSize:'12px',
+                    }
+                },
+                labels: {
+                    style: {
+                    color:'#aaa',
+                    fontSize:'10px',
+                    }
+                },
+                gridLineColor:'#f1f1f1'
+            }, {
+                title: {
+                    text: ''
+                },
+                labels: {
+                        style: {
+                        color:'#aaa',
+                        fontSize:'10px',
+                        }
+                },
+                max: 5,    
+                opposite: true
+            }],
+            legend: {
+                enabled: false
+            },
+            tooltip: {
+                shared: true
+            },
+            plotOptions: {
+                column: {
+                    grouping: false,
+                    shadow: false,
+                    borderWidth: 0,
+                    
+                }
+            },
+            exporting: {
+                enabled:false
+                },
+            series: [{
+                name: '평균처리일수',
+                    color: '#715deb',
+                data: [820, 1200, 450],
+                pointWidth:25,
+                tooltip: {
+                    valueSuffix: '일'
+                }
+            },{
+                name: '접수건수',
+                type: 'spline',
+                color: '#00bdb7',
+                data: [1, 2, 5],
+                tooltip: {
+                    valueSuffix: '건'
+                },
+                yAxis: 1
+            }]
+        });
+    }
+    spiderChart = (chartId) => {
+        Highcharts.chart(chartId, {
+            chart: {
+                    polar: true,
+                style: {
+                    fontFamily:"oneshinhan"
+                },
+            },
+            title: {
+                text: ''
+            },
+            pane: {
+                size: '80%'
+            },
+            credits: {
+                enabled: false
+            },
+            xAxis: {
+                categories: [
+                    '모집',
+                    '유지',
+                    '재지급',
+                    '보험금',
+                    '서비스 기타',
+                ],
+                tickmarkPlacement: 'on',
+                lineWidth: 0,
+                labels: {
+                    style: {
+                    color:'#666',
+                    fontSize:'13px'
+                    }
+                }
+            },
+            yAxis: {
+                gridLineInterpolation: 'polygon',
+                lineWidth: 0,
+                min: 0,
+                labels: {
+                        enabled:false
+                    }
+                
+            },
+            legend: {
+                enabled: false
+            },
+            tooltip: {
+                shared: true
+            },
+            exporting: {
+                enabled:false
+                },
+                plotOptions: {
+                    area: {
+                        fillOpacity: 0.15
+                    }
+                },
+            series: [{
+                name: '발생현황',
+                    type: 'area',
+                    color: '#715deb',
+                data: [120, 200, 50,90,156 ],
+                tooltip: {
+                    valueSuffix: '건'
+                },
+                pointPlacement: 'on'
+            }],
+        });
+    }
+    lineChart = (chartId) => {
+        Highcharts.chart(chartId, {
+            chart: {
+                style: {
+                    fontFamily:"oneshinhan"
+                },
+                plotBackgroundColor:'#f9f9f9',
+                margin: [0, 1, 30, 1]
+            },
+            title: {
+                text: ''
+            },
+            credits: {
+                enabled: false
+            },
+            xAxis: {
+                categories: ['6월', '7월', '8월', '9월'],
+                lineColor:'#e3e3e3',
+                tickWidth: 0,
+                labels: {
+                    rotation: 0,
+                    style: {
+                    color:'#919191',
+                    fontSize:'11px'
+                    }
+                },
+            },
+            yAxis: {
+                    title: {
+                        text: ''
+                },
+                max:20,
+                min:0,
+                    lineColor:'#e3e3e3',
+                    lineWidth:1,
+                gridLineWidth: 1,
+                minorTickInterval: 7,
+                minorTickLength: 0,
+                labels: {
+                        enabled: false
+                },
+            },
+            legend: {enabled: false},
+            tooltip: {
+                formatter: function () {
+                        return this.series.name + ':' + this.y + '건';
+                },
+                padding:13,
+                style: {
+                    color:'#444',
+                    fontSize:'11px'                	
+                }
+            },
+            plotOptions: {
+                line: {
+                    dataLabels: {
+                        enabled: true,
+                        style: {
+                                color:'#444',
+                                fontSize:'11px',
+                                fontWeight:'bold',
+                            }
+                        }
+                    
+                }
+            },
+            exporting: {
+                enabled:false
+                },
+                series: [{
+                    name: '최근 4개월',
+                    color: '#9e92ed',
+                    data: [3, 12, 5, 5],
+                    marker: {
+                        symbol: 'url(../../../page/images/main/ico_symbol_graph.png)'
+                    }
+                    }]
+        });
+    }
+}
+
